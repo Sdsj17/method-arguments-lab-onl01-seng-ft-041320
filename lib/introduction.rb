d408021e3abd6a305743f4dedcd2ce510c988612
Steven Springs Jr <sdsj17@yahoo.com>
2020-04-25 05:25:41 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,7 +1,10 @@
 def introduction(name)
-   puts "Hi, my name is Josh"
+   puts "Hi, my name is (name)."
 end
+
+introduction(Josh)
 
 def _introduction_with_language(name,language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
+introduction_with_language(Dan, Ember.js)
