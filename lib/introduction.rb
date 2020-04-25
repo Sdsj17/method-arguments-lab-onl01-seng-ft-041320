3392dc35668bc187b6d28539a713dd303d6d6486
Steven Springs Jr <sdsj17@yahoo.com>
2020-04-25 05:10:31 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,1 +1,7 @@
+def introduction(name)
+   puts "Hi, my name is Josh"
+end
 
+def _introduction_with_language(name,language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
